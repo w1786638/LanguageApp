8594f712804de93034a53ccf6344ed53c6c35706
Eugenie Ahangama <w1786638@my.westminster.ac.uk>
2022-05-04 18:33:36 +0100

    feat: Teacher user can pick from the many options shown on the menu

--- src/main/java/LanguageApp/TeacherMenu.java
+++ src/main/java/LanguageApp/TeacherMenu.java
@@ -7,7 +7,7 @@ package LanguageApp;
 
 /**
  *
- * @author nugge
+ * @author Eugenie Ahangama(w1786638)
  */
 public class TeacherMenu extends javax.swing.JFrame {
 
@@ -31,10 +31,12 @@ public class TeacherMenu extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        viewStudentStatsBtn = new javax.swing.JButton();
+        teacherStatisticsBtn = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        teacherAccountBtn = new javax.swing.JButton();
+        viewStudentTeacherPairStatsBtn = new javax.swing.JButton();
+        viewStudentStudentPairStatsBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,55 +51,93 @@ public class TeacherMenu extends javax.swing.JFrame {
         jButton2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton2.setText("View Conversations");
 
-        jButton3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton3.setText("View Student Statistics");
+        viewStudentStatsBtn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        viewStudentStatsBtn.setText("View Student Statistics");
+        viewStudentStatsBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewStudentStatsBtnActionPerformed(evt);
+            }
+        });
 
-        jButton4.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton4.setText("View Your Statistics");
+        teacherStatisticsBtn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        teacherStatisticsBtn.setText("View Your Statistics");
+        teacherStatisticsBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                teacherStatisticsBtnActionPerformed(evt);
+            }
+        });
 
         jButton6.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton6.setText("Start Conversation");
 
-        jButton5.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jButton5.setText("View Your Account");
+        teacherAccountBtn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        teacherAccountBtn.setText("View Your Account");
+        teacherAccountBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                teacherAccountBtnActionPerformed(evt);
+            }
+        });
+
+        viewStudentTeacherPairStatsBtn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        viewStudentTeacherPairStatsBtn.setText("View Student-Teacher Pair Statistics");
+        viewStudentTeacherPairStatsBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewStudentTeacherPairStatsBtnActionPerformed(evt);
+            }
+        });
+
+        viewStudentStudentPairStatsBtn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        viewStudentStudentPairStatsBtn.setText("View Student-Student Pair Statistics");
+        viewStudentStudentPairStatsBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewStudentStudentPairStatsBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(167, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(88, 88, 88))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(65, 65, 65)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(viewStudentTeacherPairStatsBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(viewStudentStatsBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(teacherStatisticsBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton6, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(teacherAccountBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(viewStudentStudentPairStatsBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(48, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(52, 52, 52)
+                .addGap(27, 27, 27)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(viewStudentStatsBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(viewStudentTeacherPairStatsBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(teacherStatisticsBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(viewStudentStudentPairStatsBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 33, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(104, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(teacherAccountBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -113,6 +153,51 @@ public class TeacherMenu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void viewStudentTeacherPairStatsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewStudentTeacherPairStatsBtnActionPerformed
+        // TODO add your handling code here:
+        // Open StudentTeacherStatistics here
+        StudentTeacherStatistics sts = new StudentTeacherStatistics();
+        sts.show();     // Display StudentTeacherStatistics here
+        
+        dispose();      // Close current form
+    }//GEN-LAST:event_viewStudentTeacherPairStatsBtnActionPerformed
+
+    private void viewStudentStatsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewStudentStatsBtnActionPerformed
+        // TODO add your handling code here:
+        // Open StudentStatistics here
+        StudentStatistics ss = new StudentStatistics();
+        ss.show();         // Display StudentStatistics here
+        
+        dispose();          // Close current form
+    }//GEN-LAST:event_viewStudentStatsBtnActionPerformed
+
+    private void teacherStatisticsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_teacherStatisticsBtnActionPerformed
+        // TODO add your handling code here:
+        // Open TeacherStatistics here
+        TeacherStatistics ts = new TeacherStatistics();
+        ts.show();          // Display TeacherStatistics here
+        
+        dispose();          // Close current form
+    }//GEN-LAST:event_teacherStatisticsBtnActionPerformed
+
+    private void teacherAccountBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_teacherAccountBtnActionPerformed
+        // TODO add your handling code here:
+        // Open TeacherAccount here
+        TeacherAccount ta = new TeacherAccount();
+        ta.show();      // Display TeacherAccount here
+        
+        dispose();      // Close current form
+    }//GEN-LAST:event_teacherAccountBtnActionPerformed
+
+    private void viewStudentStudentPairStatsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewStudentStudentPairStatsBtnActionPerformed
+        // TODO add your handling code here:
+        // Open StudentStudentStatistics here
+        StudentStudentStatistics sss = new StudentStudentStatistics();
+        sss.show();         // Display StudentStudentStatistics here
+        
+        dispose();          // Close current form
+    }//GEN-LAST:event_viewStudentStudentPairStatsBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -152,11 +237,13 @@ public class TeacherMenu extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton teacherAccountBtn;
+    private javax.swing.JButton teacherStatisticsBtn;
+    private javax.swing.JButton viewStudentStatsBtn;
+    private javax.swing.JButton viewStudentStudentPairStatsBtn;
+    private javax.swing.JButton viewStudentTeacherPairStatsBtn;
     // End of variables declaration//GEN-END:variables
 }
