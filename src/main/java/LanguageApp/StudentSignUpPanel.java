3672f62f7c0e25a08343617f91211942a54297c7
Eugenie Ahangama <w1786638@my.westminster.ac.uk>
2022-05-04 20:39:51 +0100

    feat: New student user will input their details to create their new account if they do not have an account already

--- src/main/java/LanguageApp/StudentSignUpPanel.java
+++ src/main/java/LanguageApp/StudentSignUpPanel.java
@@ -5,6 +5,8 @@
  */
 package LanguageApp;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Eugenie Ahangama(w1786638)
@@ -29,13 +31,13 @@ public class StudentSignUpPanel extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        studentUsername = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        studentFullName = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        studentEmailAddress = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        studentPassword = new javax.swing.JPasswordField();
         studentSignInBtn = new javax.swing.JButton();
         studentForgotPasswordBtn = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
@@ -94,10 +96,10 @@ public class StudentSignUpPanel extends javax.swing.JFrame {
                     .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jTextField1)
-                    .addComponent(jTextField2)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-                    .addComponent(jTextField3)
+                    .addComponent(studentUsername)
+                    .addComponent(studentEmailAddress)
+                    .addComponent(studentPassword, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+                    .addComponent(studentFullName)
                     .addComponent(studentForgotPasswordBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(28, 28, 28))
             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -113,18 +115,18 @@ public class StudentSignUpPanel extends javax.swing.JFrame {
                 .addGap(52, 52, 52)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(studentUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(studentFullName, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(studentEmailAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(31, 31, 31)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(studentPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 106, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -158,6 +160,23 @@ public class StudentSignUpPanel extends javax.swing.JFrame {
 
     private void studentSignInBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentSignInBtnActionPerformed
         // TODO add your handling code here:
+        String Username=studentUsername.getText().toString();
+        String FullName=studentFullName.getText().toString();
+        String EmailAddress=studentEmailAddress.getText().toString();
+        String Password=studentPassword.getText().toString();
+        
+        if(Username.equals("")) {
+            JOptionPane.showMessageDialog (null, "Username is Madatory");
+        }
+        if(FullName.equals("")) {
+            JOptionPane.showMessageDialog (null, "Full Name is Madatory");
+        }
+        if(EmailAddress.equals("")) {
+            JOptionPane.showMessageDialog (null, "Email Address is Madatory");
+        }
+        if(Password.equals("")) {
+            JOptionPane.showMessageDialog (null, "Password is Madatory");
+        }
         // Open StudentLoginPanel
         StudentLoginPanel slp = new StudentLoginPanel();
         slp.show();       // Display StudentLoginPanel here
@@ -207,11 +226,11 @@ public class StudentSignUpPanel extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField studentEmailAddress;
     private javax.swing.JButton studentForgotPasswordBtn;
+    private javax.swing.JTextField studentFullName;
+    private javax.swing.JPasswordField studentPassword;
     private javax.swing.JButton studentSignInBtn;
+    private javax.swing.JTextField studentUsername;
     // End of variables declaration//GEN-END:variables
 }
